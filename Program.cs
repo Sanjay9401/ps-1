546721685254b6bfecdaf97e5f8fba6ccadc6792
sanjay s <sanjaynetaji08341@gmail.com>
2022-08-19 09:31:09 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+public class SwapExample
+{
+    public static void Main(string[] args)
+    {
+        int a = 5, b = 6;
+        Console.WriteLine("a= " + a + " b= " + b);
+        a = a * b;
+        b = a / b;
+        a = a / b;
+        Console.Write("a= " + a + " b= " + b);
+    }
+}
